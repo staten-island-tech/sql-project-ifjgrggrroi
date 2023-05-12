2abc774592f1b449777acb9dc96466e24a999af1
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2023-05-12 10:16:02 -0400

    3

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -4,17 +4,6 @@ import { supabase } from '../lib/supabaseClient'
 
 const Data = ref('')
 
-/* async function getEmployees() {
-  try {
-    let { data: employees, error } = await supabase.from('employees').select('*')
-
-    Data.value = employees
-    console.log(employees)
-  } catch (error) {
-    console.log(error)
-  }
-} */
-
 async function getData() {
   try {
     let { data: alldata, error } = await supabase.from('alldata').select('*')
@@ -25,10 +14,11 @@ async function getData() {
     console.log(error)
   }
 }
-
 onMounted(() => {
   getData()
 })
+
+// this.alldata = this.alldata.filter((t) => t !== name)
 </script>
 
 <template>
@@ -38,15 +28,11 @@ onMounted(() => {
       <h2>Employee ID : {{ alldata.id }}</h2>
       <h2>Employee Location : {{ alldata.location }}</h2>
       <h2>Employee Pay : ${{ alldata.pay }}</h2>
+      <button class="btn">FIRE</button>
     </div>
   </div>
 </template>
 
-<!-- <ul>
-    <li v-for="employee in Data" :key="employee.id">
-      ID : {{ employee.id }}, Name {{ employee.name }}
-    </li>
-  </ul> -->
 <style>
 .parent {
   display: flex;
@@ -62,6 +48,7 @@ onMounted(() => {
   flex-direction: row;
   align-items: center; */
 }
+
 .card {
   font-size: 16px;
   width: 32rem;
@@ -71,5 +58,12 @@ onMounted(() => {
   padding: 1rem;
   text-align: center;
   border-radius: 2rem;
+}
+
+.btn {
+  border-radius: 2rem;
+  font-size: 2rem;
+  background-color: aliceblue;
+  padding: 5px 15px 5px 15px;
 }
 </style>
