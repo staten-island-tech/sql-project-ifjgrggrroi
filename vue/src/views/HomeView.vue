77505e2e7269b0fe68095b45bdf02614c816cf3c
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2023-05-16 09:55:18 -0400

    5

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -18,11 +18,10 @@ async function getData() {
 
 async function fire() {
   try {
-    let { data: alldata, error } = await supabase.from('alldata').delete().eq('id', '6')
+    let { data: alldata, error } = await supabase.from('alldata').delete().eq('id', '5')
+    getData()
     Data.value = alldata
     console.log(alldata)
-
-    console.log('hello')
   } catch (error) {
     console.log(error)
   }
@@ -43,25 +42,33 @@ const {(data, error)} = await supabase.from('alldata').delete().eq('name',
 'annie sr') -->
 
 <template>
+  <h1 class="header">Employee profiles</h1>
+
   <div class="parent">
     <div class="card" v-for="alldata in Data" :key="alldata.id">
-      <h1>Employee Name : {{ alldata.name }}</h1>
-      <h2>Employee ID : {{ alldata.id }}</h2>
-      <h2>Employee Location : {{ alldata.location }}</h2>
-      <h2>Employee Pay : ${{ alldata.pay }}</h2>
+      <h1>Name : {{ alldata.name }}</h1>
+      <h2>Location : {{ alldata.location }}</h2>
+      <h2>ID : {{ alldata.id }}</h2>
+      <h2>Pay : ${{ alldata.pay }}</h2>
       <button @click="fire()" class="btn">FIRE</button>
     </div>
   </div>
 </template>
 
 <style>
+.header {
+  font-size: 6.5rem;
+  margin-top: 2.5rem;
+  text-align: center;
+}
+
 .parent {
   display: flex;
   /* justify-content: space-between; */
   align-items: flex-start;
   flex-direction: row;
   flex-wrap: wrap;
-  margin: 5rem auto;
+  margin: auto;
   width: 100%;
   text-align: center;
 
